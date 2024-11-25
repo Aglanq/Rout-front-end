cabc0a8df02a6ad7b89726efad4540ab422bbe01
Aglanq <mahmoudaglan2003@gmail.com>
2024-11-25 16:56:04 +0200

    Add files via upload

---
+++ ASS_2_JS/quotes.js
@@ -0,0 +1,33 @@
+const arrayOfQuotes = [
+    {'author': 'Jim Rohn', 
+     'quote': 'Beware of what you become in pursuit of what you want.'
+    },
+    {'author': 'Epictetus', 
+     'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Oscar Wilde', 
+     'quote': 'Be yourself; everyone else is already taken.'
+    },
+    {'author': 'Nelson Mandela', 
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'Do not take life too seriously. You will not get out alive.'
+    },
+    {'author': ' Frank Zappa', 
+     'quote': 'So many books, so little time.'
+    },
+    {'author': ' Marcus Tullius Cicero', 
+     'quote': 'A room without books is like a body without a soul.'
+    },
+];
+
+function generateQuote(){
+    const random = Number.parseInt(Math.random()*arrayOfQuotes.length + 1);
+    document.querySelector('#quoteOutput').textContent = `\"${arrayOfQuotes[random].quote}\"`;
+    document.querySelector('#authorOutput').textContent = `--${arrayOfQuotes[random].author}`;
+    
+}
